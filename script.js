8c50e818cf96bda2e02ccaa3ccfd3d86260bb1a4
Andrew Peattie <drewpeattie@hotmail.com>
2021-12-01 14:18:14 +0000

    session storage for first load and refresh

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
 'use strict';
-
-//// * GENERAL ////
 const header = document.querySelector('.header');
 const section1 = document.querySelector('#section--1');
 const allSections = document.querySelectorAll('.section');
 const headerScrollBtn = document.querySelector('.hero_btn');
 const footerScrollBtn = document.querySelector('.footer_btn');
-//// * NAV ////
 const navbar = document.querySelector('.navbar');
 const navContainer = document.querySelector('.nav_container');
 const mobileMenu = document.querySelector('#mobile_menu');
 const navMenu = document.querySelector('.nav_menu');
 const navLinks = document.querySelectorAll('.nav_links');
-//// * MODAL ////
+const imgTargets = document.querySelectorAll('img[data-src]');
 const modal = document.getElementById('email_modal');
-const openModalBtn = document.querySelector('.nav_links_btn');
+const openNavModalBtn = document.querySelector('.nav_links_btn');
+const openMemberModalBtn = document.querySelector('.membership_sign_up');
 const closeModalBtn = document.querySelector('.close_btn');
-//// * GALLERY ////
 const modalGallery = document.querySelector('.gallery_overlay');
-const services = document.querySelectorAll('.service_content');
+const allServices = document.querySelectorAll('.service_content');
 const closeGalleryBtn = document.querySelector('.close_btn_gallery');
-//// * FORM ////
 const form = document.getElementById('form');
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
-//// * TAB ////
 const tabs = document.querySelectorAll('.memberships_tab');
 const tabsContainer = document.querySelector('.memberships_tab_container');
 const tabsContent = document.querySelectorAll('.memberships_content');
 
 // ? Semantic ( gallery alt) ++
-// ? Sections Color
 // ? Form error link
-// ? On page refresh opacity 1 ( SESSION STORAGE )
 // ? Scroll animation mobile
+// ? lazy image refresh
+// ? TIDY CODE & NAMES
+// ? modal escp key
+// ? gallery images
+// ! whast a website needs
+
+// ! TIDY NAMES & CODE ( HTML CSS JS)
+
 
 
 //// * Mobile Menu Toggle ////
@@ -69,62 +70,44 @@ const stickyNav = (entries) => {
 };
 
 const headerObserver = new IntersectionObserver(stickyNav, {
-  root: null, // Entire view port
-  threshold: 0.1, // 10% of the header reveal nav
-  // Inspect - intersectionRatio & isIntersecting
+  root: null,
+  threshold: 0.1,
 });
 headerObserver.observe(header);
 
 //// * SECTION REVEAL ////
-const revealSection = function (entries, observer) {
+const revealSection = (entries, observer) => {
   const [entry] = entries;
 
-  if (entry.isIntersecting) {
-    entry.target.classList.remove('section--hidden');
-    observer.unobserve(entry.target);
-  }
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.1,
+  threshold: 0.15,
 });
 
-allSections.forEach((section) => {
-  sectionObserver.observe(section);
-  section.classList.add('section--hidden');
-});
+if (sessionStorage.getItem("isNewSession")) {
+  allSections.forEach((section) => {
+    sectionObserver.observe(section);
+    section.classList.add('section--onload');
+  });
+} else {
+  allSections.forEach((section) => {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden');
+  });
 
-// ! ADD NEW CLASS LIST POST LOAD
-
-// if (sessionStorage.getItem("isNewSession")) {
-//   console.log('welcome back');
-// } else {
-//   console.log('FIRST TIME');
-//   sessionStorage.setItem('isNewSession', 'true');
-// }
-
-// if (sessionStorage.getItem("isNewSession")) {
-//   allSections.forEach((section) => {
-//     section.style.transition = "all 0s";
-//     section.style.opacity = "1";
-//   });
-// } else {
-//   allSections.forEach((section) => {
-//     section.classList.add('section--hidden');
-//     sessionStorage.setItem('isNewSession', 'true');
-//   });
-// }
-
-
-
+  sessionStorage.setItem('isNewSession', 'true');
+};
 
 //// * SCROLL VIEW ////
 const scrollSectionHandler = (location) => location.scrollIntoView({ behavior: 'smooth' });
-
 headerScrollBtn.addEventListener('click', () => scrollSectionHandler(section1))
 footerScrollBtn.addEventListener('click', () => scrollSectionHandler(header))
-
 
 //// * NAV SCROLL LINKS ////
 navLinks.forEach((link) => {
@@ -145,19 +128,17 @@ navLinks.forEach((link) => {
 });
 
 //// * LAZY IMAGES ////
-const imgTargets = document.querySelectorAll('img[data-src]');
-
-const loadImg = function (entries, observer) {
+const loadImg = (entries, observer) => {
   const [entry] = entries;
   if (!entry.isIntersecting) return;
 
-  // Replace src with data-src
+  // Replace img src with data-src
   entry.target.src = entry.target.dataset.src;
 
-  entry.target.addEventListener('load', function () {
+  entry.target.addEventListener('load', () => {
     entry.target.classList.remove('lazy_img');
-
   });
+
   observer.unobserve(entry.target);
 };
 
@@ -169,32 +150,33 @@ const imgObserver = new IntersectionObserver(loadImg, {
 
 imgTargets.forEach(img => imgObserver.observe(img));
 
-//// * MODAL & SERVICE GALLERY ////
-const openModal = function (modalType, evt) {
+//// * MODAL'S & SERVICE GALLERY ////
+const openModal = function (modal, evt) {
   evt.preventDefault()
-  modalType.style.display = 'block';
+  modal.style.display = 'block';
   document.body.style.overflow = 'hidden';
 };
 
-openModalBtn.addEventListener('click', (evt) => openModal(modal, evt));
-services.forEach((service) => service.addEventListener('click', (evt) => openModal(modalGallery, evt)));
+openNavModalBtn.addEventListener('click', (evt) => openModal(modal, evt));
+openMemberModalBtn.addEventListener('click', (evt) => openModal(modal, evt));
+allServices.forEach((service) => service.addEventListener('click', (evt) => openModal(modalGallery, evt)));
 
-const closeModal = function (close) {
-  close.style.display = 'none';
+const closeModal = function (modal) {
+  console.log(modal);
+  modal.style.display = 'none';
   document.body.style.overflowY = 'scroll';
 };
+
 
 closeModalBtn.addEventListener('click', () => closeModal(modal));
 closeGalleryBtn.addEventListener('click', () => closeModal(modalGallery));
 
-const escapeClose = function (evt, modalType) {
-  if (evt.key === 'Escape' && !modalType.style.display === 'none') {
-    closeModal();
-  }
-};
 
-document.addEventListener('keydown', () => closeModal(modal));
-document.addEventListener('keydown', () => closeModal(modalGallery));
+window.addEventListener('keydown', function (event) {
+  if (event.key == 'Escape') {
+    closeModal(modal, modalGallery, modal);
+  }
+});
 
 window.addEventListener('click', (e) => {
   if (e.target === modal || e.target === modalGallery) {
@@ -207,19 +189,15 @@ window.addEventListener('click', (e) => {
 });
 
 //// * MEMBER TAB ////
-tabsContainer.addEventListener('click', function (evt) {
+tabsContainer.addEventListener('click', (evt) => {
   const clicked = evt.target.closest('.memberships_tab');
-
   // Guard clause
   if (!clicked) return;
-
   // Remove active classes
   tabs.forEach(tab => tab.classList.remove('memberships_tab--active'));
   tabsContent.forEach(cont => cont.classList.remove('memberships_content--active'));
-
   // Activate tab
   clicked.classList.add('memberships_tab--active');
-
   // Activate content area
   document.querySelector(`.memberships_content--${clicked.dataset.tab}`)
     .classList.add('memberships_content--active');
@@ -236,7 +214,7 @@ const showError = (input, msg) => {
 
 //// ! FORM REQUIRED FIELDS ////
 function checkRequired(inputArr) {
-  inputArr.forEach(function (input) {
+  inputArr.forEach(input => {
     if (input.value.trim() === '') showError(input, `* ${getFieldName(input)} is required`);
   })
 }
@@ -267,9 +245,9 @@ async function handleSubmit(evt) {
     }
   }).then(response => {
     if (response.ok) {
-      status.innerHTML = "Thanks for your submission!";
-    } else {
       checkRequired()
+    } else {
+      status.innerHTML = "Thanks for your submission!";
     }
     form.reset()
   }).catch(error => {
